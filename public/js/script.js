a3b1e4f8108a5cdb42ce8b362329e38bd1c96c97
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-01-17 16:01:46 +0530

    capus search

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,17 @@ $(document).ready(function () {
     mobileHamburger.addEventListener('click', () => {
         leftBar.classList.toggle('left-sidebar-open');
     })
-
-
 })
+
+
+function delay(callback, ms) {
+    var timer = 0;
+    return function () {
+        var context = this,
+            args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            callback.apply(context, args);
+        }, ms || 0);
+    };
+}
