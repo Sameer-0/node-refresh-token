1776e75b6ea9efc20fe2dd638967c1fc075f0d8c
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-01-28 16:52:04 +0530

    logg

--- router.js
+++ router.js
@@ -8,7 +8,7 @@ function setRouter(app) {
     
     app.use('/', homeRouter)
     app.use('/user', userRouter)
-    app.use('/management', isLoggedIn, management)
+    app.use('/management', management)
 //    app.use('/management', isLoggedIn,  management)
 }
 
