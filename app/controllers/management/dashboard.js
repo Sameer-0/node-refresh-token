4c3ca6a33964164f283da2f2d80c02a0f419c6c7
Rajesh Pillai <pillai.rajesh7@gmail.com>
2022-02-04 13:30:18 +0530

    Multi-Step form work started

--- app/controllers/management/dashboard.js
+++ app/controllers/management/dashboard.js
@@ -1,6 +1,21 @@
+const AcademicYear = require('../../models/AcademicYear')
+const Buildings = require('../../models/Buildings')
+const OrganizationMaster = require("../../models/OrganizationMaster")
+const CampusMaster = require("../../models/CampusMaster")
+const SlotIntervalTimings = require("../../models/SlotIntervalTimings")
+
 module.exports = {
 
     getDashboard: (req, res) => {
-        res.render('management/dashboard')
+
+        Promise.all([Buildings.fetchAll(10), OrganizationMaster.fetchAll(50), CampusMaster.fetchAll(50), SlotIntervalTimings.fetchAll(50), Buildings.getCount()]).then(result => {
+            res.render('management/dashboard', {
+                buildingList: result[0].recordset,
+                orgList: result[1].recordset,
+                campusList: result[2].recordset,
+                timeList: result[3].recordset,
+                pageCount: result[4].recordset[0].count
+            })
+        })
     }
 }
