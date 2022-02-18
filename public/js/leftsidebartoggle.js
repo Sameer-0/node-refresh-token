9a825d15fd3d4096eea27f3642b5b51311fb6446
Rajesh Pillai <pillai.rajesh7@gmail.com>
2022-02-18 18:24:58 +0530

    Custom Notification and alert included

--- public/js/leftsidebartoggle.js
+++ public/js/leftsidebartoggle.js
@@ -1,4 +1,4 @@
-$('.left-sidebar-toggle').click(function() {
+$('.left-sidebar-toggle').click(function () {
     console.log('leftsidebar pressed')
     let leftbar = $('.left-sidebar');
     leftbar.toggleClass('left-400');
@@ -11,4 +11,46 @@ $('.left-sidebar-toggle').click(function() {
 
     let btn = $('.left-sidebar-toggle');
     btn.toggleClass('left-sidebar-toggle-turn')
+})
+
+var notification = $(".notification-content");
+
+
+// var btn = document.getElementById("myBtn");
+
+
+var close = $(".notification-close");
+
+
+function notify() {
+
+    notification.addClass('notification-active');
+    $('.notification-content').css('display', 'flex');
+
+}
+
+
+$(".notification-close").on('click', function () {
+    notification.removeClass('notification-active')
+    location.reload();
+})
+
+
+
+
+// Get the <button> element that closes the modal
+var close = $('.close');
+
+// When the user clicks on the button, open the modal
+function alertMsg() {
+    $('#alertModal').css('display', 'block')
+
+
+}
+
+
+// When the user clicks on <button> (x), close the modal
+
+$('.close').on('click', function () {
+    $('#alertModal').css('display', 'none');
 })
