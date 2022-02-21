4f34b9b8a66f3efb7fc81ce1bacfec5f8bf25d8f
technologydynamite@gmail.com <suraj.cartgeek@gmail.com>
2022-02-21 22:47:43 +0530

    code optimization in dashboard

--- app/routers/management/room.js
+++ app/routers/management/room.js
@@ -8,6 +8,7 @@ const {
 const roomcontroller = require('../../controllers/management/rooms')
 const roomslotscontroller = require("../../controllers/management/roomslots")
 const roomtype = require('../../controllers/management/roomtype')
+const bookings = require('../../controllers/management/bookings')
 const validate = require('../../middlewares/validate')
 const roomValidate = require('../../middlewares/roomValidate')
 
@@ -31,5 +32,11 @@ router.patch('/rooms/roomtypes', roomtype.deleteAll)
 
 //ROOM SLOTS ROUTER
 router.get('/room/slots', roomslotscontroller.getPage)
+
+
+//ROOM BOOKING
+
+router.get('/rooms/bookings', bookings.getPage)
+
 
 module.exports = router;
