5ad9ab8fe8d82886783b32ed9729dc919e47dd4b
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-03-17 13:12:15 +0530

    view details for room transactions

---
+++ app/controllers/admin/room-transactions/details.js
@@ -0,0 +1,23 @@
+const {
+    check,
+    oneOf,
+    validationResult
+} = require('express-validator');
+
+const RoomTransactionDetails = require('../../../models/RoomTransactionDetails')
+
+module.exports = {
+
+    findByLid: (req, res) => {
+        console.log('req::::::::::::::::::::', req.query.id)
+        RoomTransactionDetails.findByRoomTransactionId(req.query.id, res.locals.slug).then(result => {
+            res.status(200).json({
+                status: 200,
+                data: result.recordset,
+                message:'success'
+            })
+            console.log(result.recordset)
+        })
+    }
+
+}
--- app/routers/admin/room-transacton.js
+++ app/routers/admin/room-transacton.js
@@ -7,9 +7,17 @@ const {
 
 const validate = require('../../middlewares/validate')
 const transactions =  require('../../controllers/admin/room-transactions/index')
+const details =  require('../../controllers/admin/room-transactions/details')
+
 
 router.get('/room-transactions', transactions.getPage)
 router.get('/room-transactions/search', transactions.search)
 router.post('/room-transactions/pagination', transactions.pagination)
 router.post('/room-transactions', validate('JsonValidator'),  transactions.create)
+
+
+
+
+//ROOM TRANSACTION DETAILS
+router.get('/room-transactions/details', details.findByLid)
 module.exports = router
