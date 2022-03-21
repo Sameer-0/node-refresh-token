2b51e6d08c25b8e4c7325d34921b96cda39ab6bd
Rajesh Pillai <pillai.rajesh7@gmail.com>
2022-03-21 17:58:29 +0530

    Faculty work page started

--- app/models/ProgramSessions.js
+++ app/models/ProgramSessions.js
@@ -7,9 +7,8 @@ const {
 module.exports = class {
     static fetchAll(rowcount, slug) {
         return poolConnection.then(pool => {
-            return pool.request().query(`SELECT TOP ${Number(rowcount)} ps.id, p.program_name, adc.acad_session, ps.active FROM [${slug}].program_sessions ps 
+            return pool.request().query(`SELECT TOP ${Number(rowcount)} ps.id, p.program_name, p.program_code, adc.acad_session, ps.active FROM [${slug}].program_sessions ps 
             INNER JOIN [${slug}].programs p ON ps.program_lid = p.id
-            INNER JOIN [${slug}].programs p ON ps.program_lid = p.program_code
             INNER JOIN [dbo].acad_sessions adc ON adc.id = ps.acad_session_lid
             WHERE ps.active = 1 AND p.active = 1 AND adc.active = 1 ORDER BY ps.id DESC`)
         })
@@ -19,7 +18,7 @@ module.exports = class {
         return poolConnection.then(pool => {
             let request = pool.request()
             return request.input('pageNo', sql.Int, pageNo)
-                .query(`SELECT ps.id, p.program_name, adc.acad_session, ps.active FROM [${slug}].program_sessions ps 
+                .query(`SELECT ps.id, p.program_name, p.program_code, adc.acad_session, ps.active FROM [${slug}].program_sessions ps 
                 INNER JOIN [${slug}].programs p ON ps.program_lid = p.id
                 INNER JOIN [dbo].acad_sessions adc ON adc.id = ps.acad_session_lid
                 WHERE ps.active = 1 AND p.active = 1 AND adc.active = 1 ORDER BY ps.id DESC  OFFSET (@pageNo - 1) * 10 ROWS FETCH NEXT 10 ROWS ONLY`)
@@ -29,7 +28,7 @@ module.exports = class {
     static search(rowcount, keyword, slug) {
         return poolConnection.then(pool => {
             return pool.request().input('keyword', sql.NVarChar(100), '%' + keyword + '%')
-                .query(`SELECT TOP ${Number(rowcount)} ps.id, p.program_name, adc.acad_session, ps.active FROM [${slug}].program_sessions ps 
+                .query(`SELECT TOP ${Number(rowcount)} ps.id, p.program_name, p.program_code, adc.acad_session, ps.active FROM [${slug}].program_sessions ps 
                 INNER JOIN [${slug}].programs p ON ps.program_lid = p.id
                 INNER JOIN [dbo].acad_sessions adc ON adc.id = ps.acad_session_lid
                 WHERE ps.active = 1 AND p.active = 1 AND adc.active = 1  AND (p.program_name LIKE @keyword OR adc.acad_session LIKE @keyword) ORDER BY ps.id DESC`)
