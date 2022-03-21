caa8de60cf4af4e3fa64ba4af21cac6ca1839ae3
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-03-21 13:42:50 +0530

    course workload search|pagination|switch done

--- app/routers/admin/courseworkload.js
+++ app/routers/admin/courseworkload.js
@@ -9,4 +9,8 @@ const courseworkload =  require('../../controllers/admin/courseworkload/index')
 
 router.get('/courseworkload', courseworkload.getPage)
 router.post('/courseworkload', courseworkload.fetchFromSAP)
+router.post('/courseworkload/changestatus', courseworkload.changestatus)
+router.get('/courseworkload/getAll', courseworkload.getAll)
+router.get('/courseworkload/search', courseworkload.search)
+router.post('/courseworkload/pagination', courseworkload.pagination)
 module.exports = router
