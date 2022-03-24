fff0cc08b1dc59aa75faa76be055997b694b22a5
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-03-24 17:18:30 +0530

    working on faculty work

--- app/models/FacultyWorks.js
+++ app/models/FacultyWorks.js
@@ -9,7 +9,7 @@ module.exports = class {
         return poolConnection.then(pool => {
             return pool.request().query(`SELECT TOP ${Number(rowcont)} fw.id, fw.faculty_lid, fw.program_session_lid, fw.module_lid, fw.lecture_per_week, fw.practical_per_week, fw.tutorial_per_week, fw.workshop_per_week, fw.is_batch_preference_set,icw.module_name,f.faculty_id, f.faculty_name, ps.program_lid,acs.acad_session, fw.active
             FROM [${slug}].faculty_works fw 
-            INNER JOIN [${slug}].[initial_course_workload] icw ON icw.module_id = fw.module_lid
+            INNER JOIN [${slug}].[initial_course_workload] icw ON icw.id = fw.module_lid
             INNER JOIN [${slug}].faculties f ON f.id = fw.faculty_lid
             INNER JOIN [${slug}].program_sessions ps ON ps.id = fw.program_session_lid 
             INNER JOIN [dbo].acad_sessions acs ON acs.id = ps.acad_session_lid
