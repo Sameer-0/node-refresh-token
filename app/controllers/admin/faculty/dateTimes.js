c3b812f30ea5398724788a368ce1fb0679afd183
Rajesh Pillai <pillai.rajesh7@gmail.com>
2022-03-25 11:09:49 +0530

    Faculty date time page working

--- app/controllers/admin/faculty/dateTimes.js
+++ app/controllers/admin/faculty/dateTimes.js
@@ -1,5 +1,7 @@
 const Faculties = require('../../../models/Faculties');
 const FacultyDateTimes = require('../../../models/FacultyDateTimes');
+const AcademicCalender  = require('../../../models/AcademicCalender');
+const SlotIntervalTimings  = require('../../../models/SlotIntervalTimings');
 const {
     check,
     oneOf,
@@ -9,12 +11,14 @@ const {
 module.exports = {
     getPage: (req, res) => {
 
-        Promise.all([FacultyDateTimes.fetchAll(10, res.locals.slug), FacultyDateTimes.getCount(res.locals.slug), Faculties.fetchAll(1000, res.locals.slug)]).then(result => {
+        Promise.all([FacultyDateTimes.fetchAll(10, res.locals.slug), FacultyDateTimes.getCount(res.locals.slug), Faculties.fetchAll(1000, res.locals.slug), AcademicCalender.fetchAll(1000), SlotIntervalTimings.fetchAll(1000)]).then(result => {
       
             res.render('admin/faculty/facultydatetime', {
                 FacultyDateTimeList: result[0].recordset,
                 pageCount: result[1].recordset[0].count,
-                faculties: result[2].recordset
+                facultyList: result[2].recordset,
+                AcademicCalenderList: result[3].recordset,
+                SlotIntervalTimingsList: result[4].recordset
             })
         })
 
