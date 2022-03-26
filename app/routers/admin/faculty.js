24152553170679363d721df91416d52e569bcb5e
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-03-26 11:21:06 +0530

    26-03-2022

--- app/routers/admin/faculty.js
+++ app/routers/admin/faculty.js
@@ -8,6 +8,7 @@ const {
 const index =  require('../../controllers/admin/faculty/index')
 const works =  require('../../controllers/admin/faculty/works')
 const dateTimes = require('../../controllers/admin/faculty/dateTimes')
+const preferences = require('../../controllers/admin/faculty/preferences')
 router.get('/faculties', index.getPage)
 
 router.get('/faculties/date-time', dateTimes.getPage)
@@ -18,4 +19,10 @@ router.get('/faculties/date-time', dateTimes.getPage)
 
 //faculty works
 router.get('/faculties/works', works.getPage)
+
+
+
+
+//workpreferences
+router.get('/faculties/workpreferences', preferences.getPage)
 module.exports = router
