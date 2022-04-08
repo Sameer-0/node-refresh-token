02609a3f04d6f9c130215d002eb6e6cac0101d7c
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-04-08 12:00:03 +0530

    remove static value from sql

--- app/models/FacultyDateTimes.js
+++ app/models/FacultyDateTimes.js
@@ -23,8 +23,8 @@ module.exports = class FacultyDateTimes {
         return poolConnection.then(pool => {
             return pool.request().query(`SELECT TOP ${Number(rowcount)} fdt.id, fdt.faculty_lid, fdt.start_date_id, fdt.end_date_id, fdt.start_time_id, fdt.end_time_id, 
             f.faculty_name, f.faculty_id, ac.date as start_date, ac1.date as end_date, sit.start_time, sit.end_time
-            FROM [bncp-mum].faculty_date_times fdt 
-            INNER JOIN [bncp-mum].[faculties] f ON fdt.faculty_lid =  f.id
+            FROM [${slug}].faculty_date_times fdt 
+            INNER JOIN [${slug}].[faculties] f ON fdt.faculty_lid =  f.id
 			INNER JOIN [dbo].[academic_calendar] ac ON fdt.start_date_id =  ac.id
             INNER JOIN [dbo].[academic_calendar] ac1 ON fdt.end_date_id =  ac1.id
             INNER JOIN [dbo].[slot_interval_timings] sit ON fdt.start_time_id = sit.id           
@@ -32,10 +32,10 @@ module.exports = class FacultyDateTimes {
         })
     }
 
-    static getCount() {
+    static getCount(slug) {
         return poolConnection.then(pool => {
             let request = pool.request()
-            return request.query(`SELECT COUNT(*) as count FROM [bncp-mum].faculty_date_times`)  
+            return request.query(`SELECT COUNT(*) as count FROM [${slug}].faculty_date_times`)  
         })
     }
 
