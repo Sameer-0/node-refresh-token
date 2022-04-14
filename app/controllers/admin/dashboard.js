9fc6e056f3369d1acf7a117045881fe97b1a5b95
Rajesh Pillai <pillai.rajesh7@gmail.com>
2022-04-14 12:39:28 +0530

    UI step-form working

--- app/controllers/admin/dashboard.js
+++ app/controllers/admin/dashboard.js
@@ -1,23 +1,25 @@
-const Settings = require("../../models/Settings")
-const DboDays = require("../../models/DboDays")
-const Holidays = require("../../models/Holidays")
-const Rooms = require('../../models/Rooms')
+const Settings = require("../../models/Settings");
+const DboDays = require("../../models/DboDays");
+const Holidays = require("../../models/Holidays");
+const Rooms = require('../../models/Rooms');
 const Buildings = require('../../models/Buildings');
 const Organizations = require('../../models/Organizations');
 const Divisions = require('../../models/Divisions');
 const SlotIntervalTimings = require('../../models/SlotIntervalTimings');
 const Campuses = require('../../models/Campuses');
 const AcademicCalender = require("../../models/AcademicCalender");
-const Programs = require('../../models/Programs')
-const ProgramTypes = require('../../models/programType')
-const AcadYear = require('../../models/AcademicYear')
-const AcadSession = require('../../models/AcadSession')
-const SessionTypes = require('../../models/SessionTypes')
-const CourseWorkload = require('../../models/CourseWorkload')
+const Programs = require('../../models/Programs');
+const ProgramTypes = require('../../models/programType');
+const AcadYear = require('../../models/AcademicYear');
+const AcadSession = require('../../models/AcadSession');
+const SessionTypes = require('../../models/SessionTypes');
+const CourseWorkload = require('../../models/CourseWorkload');
+const Faculties = require('../../models/Faculties');
+const FacultyDbo = require('../../models/FacultyDbo');
 
 module.exports = {
     getDashboard: (req, res) => {
-        Promise.all([Settings.fetchStepForm(res.locals.slug), DboDays.fetchAll(10), Rooms.fetchAll(100), Divisions.fetchAll(100, res.locals.slug), Organizations.fetchAll(200), Buildings.fetchAll(50), SlotIntervalTimings.fetchAll(100), Campuses.fetchAll(500), AcademicCalender.fetchAll(100), Programs.fetchAll(10, res.locals.slug), ProgramTypes.fetchAll(100, res.locals.slug), AcadYear.fetchAll(),AcadSession.fetchAll(1000), SessionTypes.fetchAll(10, res.locals.slug), AcademicCalender.fetchAll(100), CourseWorkload.fetchAll(10, res.locals.slug), Divisions.fetchDivisionData(100, res.locals.slug)]).then(result => {
+        Promise.all([Settings.fetchStepForm(res.locals.slug), DboDays.fetchAll(10), Rooms.fetchAll(100), Divisions.fetchAll(100, res.locals.slug), Organizations.fetchAll(200), Buildings.fetchAll(50), SlotIntervalTimings.fetchAll(100), Campuses.fetchAll(500), AcademicCalender.fetchAll(100), Programs.fetchAll(10, res.locals.slug), ProgramTypes.fetchAll(100, res.locals.slug), AcadYear.fetchAll(),AcadSession.fetchAll(1000), SessionTypes.fetchAll(10, res.locals.slug), AcademicCalender.fetchAll(100), CourseWorkload.fetchAll(10, res.locals.slug), Divisions.fetchDivisionData(100, res.locals.slug), Faculties.fetchAll(10, res.locals.slug), FacultyDbo.fetchAll(1000)]).then(result => {
             
             console.log('divisionData:::>>', result[16].recordset)
             res.render('admin/dashboard/index', {
@@ -38,6 +40,8 @@ module.exports = {
                 acadCal: result[14].recordset,
                 courseWorkloadList: result[15].recordset,
                 divisionData: result[16].recordset,
+                facultyList: result[17].recordset,
+                faculties: result[18].recordset,
                 path:'/admin'
             })
         }) 
