83b9fd8eee2bb793ada4259fa636fde5f3b59333
Rajesh <pillai.rajesh7@gmail.com>
2022-05-02 18:41:00 +0530

    custom Error

--- public/js/dashboardscript.js
+++ public/js/dashboardscript.js
@@ -26,12 +26,11 @@
                 if (trLength > 1) {
                     $(this).closest('tr').remove()
                 } else {
-                    alert('Cannot delete this room.')  
+                    alert('Cannot delete this room.')
                 }
             })
 
             //Add more rows for campus
-
             $('#add-more-campus').on('click', function () {
                 console.log("Organization Added::::")
 
@@ -59,7 +58,6 @@
             })
 
             //Add more rows for building    
-
             $('#add-more-building').on('click', function () {
                 console.log("Organization Added::::")
                 let lastTr = $('#add-more-building-table tbody tr:last-child')
@@ -96,45 +94,44 @@
             })
 
             //Add more rows in room
-
             $('#add-more-room').on('click', function () {
                 console.log("Room Added::::1")
-    
+
                 let lastTr = $('#add-more-room-table tbody tr:last-child')
-                // lastTr.find('.modalSelect2').select('destroy');
-    
-    
+                lastTr.find('.modalSelect2').select2('destroy');
+
+                console.log('lastTr', lastTr)
                 let roomNumber = lastTr.find(`input[name='room_number']`).val();
                 let roomTypeId = lastTr.find(`select[name='room_type_id']`).val();
                 let floorNumber = lastTr.find(`input[name='floor_number']`).val();
                 let capacity = lastTr.find(`input[name='capacity']`).val();
-                let startTimeId = lastTr.find(`input[name='start_time_id']`).val();
-                let endTimeId = lastTr.find(`input[name='end_time_id']`).val();
-                let isBasement = lastTr.find(`input[name='is_basement']`).val();
-                let isProcessed = lastTr.find(`input[name='is_processed']`).val();
-    
+                let startTimeId = lastTr.find(`select[name='start_time_id']`).val();
+                let endTimeId = lastTr.find(`select[name='end_time_id']`).val();
+                let isBasement = lastTr.find(`select[name='is_basement']`).val();
+                // let isProcessed = lastTr.find(`input[name='is_processed']`).val();
+
                 let clonedTr = lastTr.clone();
                 clonedTr.find(`input[name='room_number']`).val('')
                 clonedTr.find(`select[name='room_type_id']`).val('')
-                clonedTr.find(`select[name='floor_number']`).val('')
-                clonedTr.find(`select[name='capacity']`).val('')
+                clonedTr.find(`input[name='floor_number']`).val('')
+                clonedTr.find(`input[name='capacity']`).val('')
                 clonedTr.find(`select[name='start_time_id']`).val('')
                 clonedTr.find(`select[name='end_time_id']`).val('')
                 clonedTr.find(`select[name='is_basement']`).val('')
-                clonedTr.find(`select[name='is_processed']`).val('')
-                    
-    
+                // clonedTr.find(`select[name='is_processed']`).val('')
+
                 $('#add-more-room-table tbody').append(clonedTr)
-    
-                lastTr.find('.modalSelect2').select({
-                    dropdownParent: $('#add-more-room-modal')
+
+
+                lastTr.find('.modalSelect2').select2({
+                    dropdownParent: $('#add-room-modal')
                 });
-    
-                clonedTr.find('.modalSelect2').select({
-                    dropdownParent: $('#add-more-room-modal')
+
+                clonedTr.find('.modalSelect2').select2({
+                    dropdownParent: $('#add-room-modal')
                 });
             })
-    
+
             $('#add-more-room-table').on('click', '.remove-room', function () {
                 let trLength = $('#add-more-room-table tbody tr').length;
                 if (trLength > 1) {
@@ -148,16 +145,16 @@
 
             $('#add-more-faculty').on('click', function () {
                 console.log("Room Added::::")
-    
+
                 let lastTr = $('#add-more-faculty-table tbody tr:last-child')
-    
+
                 let facultyId = lastTr.find(`input[name='faculty_id']`).val();
                 let facultyName = lastTr.find(`select[name='faculty_name']`).val();
                 let startTimeId = lastTr.find(`input[name='start_time_id']`).val();
                 let endTimeId = lastTr.find(`input[name='end_time_id']`).val();
                 let orgLid = lastTr.find(`input[name='org_lid']`).val();
                 let campusLid = lastTr.find(`input[name='campus_lid']`).val();
-          
+
                 let clonedTr = lastTr.clone();
                 clonedTr.find(`input[name='faculty_id']`).val('')
                 clonedTr.find(`select[name='faculty_name']`).val('')
@@ -166,18 +163,18 @@
                 clonedTr.find(`select[name='org_lid']`).val('')
                 clonedTr.find(`select[name='campus_lid']`).val('')
 
-    
+
                 $('#add-more-faculty-table tbody').append(clonedTr)
-    
+
                 lastTr.find('.modalSelect2').select({
                     dropdownParent: $('#add-more-faculty-modal')
                 });
-    
+
                 clonedTr.find('.modalSelect2').select({
                     dropdownParent: $('#add-more-faculty-modal')
                 });
             })
-    
+
             $('#add-more-faculty-table').on('click', '.remove-faculty', function () {
                 let trLength = $('#add-more-faculty-table tbody tr').length;
                 if (trLength > 1) {
@@ -186,5 +183,3 @@
                     alert('Cannot delete this room.')
                 }
             })
-
-            
