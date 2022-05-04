0802c6001fba72891a9dd74bcaf3a88e7d65a7c8
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-05-04 11:22:36 +0530

    program day and program session refresh tocken

--- app/models/ProgramDays.js
+++ app/models/ProgramDays.js
@@ -16,11 +16,11 @@ module.exports = class {
         })
     }
 
-    static pagination(rowcount, pageNo, slug) {
+    static pagination(pageNo, slug) {
         return poolConnection.then(pool => {
             let request = pool.request()
             return request.input('pageNo', sql.Int, pageNo)
-                .query(`SELECT TOP ${Number(rowcount)} pd.id, pd.program_lid, pd.day_lid, IIF(pd.is_lecture = 1 ,'Yes','No') as is_lecture, p.program_name, d.day_name 
+                .query(`SELECT  pd.id, pd.program_lid, pd.day_lid, IIF(pd.is_lecture = 1 ,'Yes','No') as is_lecture, p.program_name, d.day_name 
                 FROM [${slug}].program_days pd
                 INNER JOIN [${slug}].programs p ON  pd.program_lid =  p.id  
                 INNER JOIN [${slug}].days d ON pd.day_lid = d.id
@@ -56,6 +56,17 @@ module.exports = class {
                 .input('userId', sql.TinyInt, userId)
             let stmt = `UPDATE [${slug}].program_days SET is_lecture = @Status, last_modified_by = @userId WHERE id =  @Id`
             return request.query(stmt)
+        })
+    }
+
+
+    static refresh(slug, userid) {
+        return poolConnection.then(pool => {
+            const request = pool.request();
+            return request
+                .input('last_modified_by', sql.Int, userid)
+                .output('output_json', sql.NVarChar(sql.MAX))
+                .execute(`[${slug}].[sp_refresh_program_days]`)
         })
     }
 }
