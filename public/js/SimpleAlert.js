96cc5e03f11d590d7ed25e28d6c47b3201dd3f26
Rajesh <pillai.rajesh7@gmail.com>
2022-05-09 17:41:14 +0530

    Alert notification fix

--- public/js/SimpleAlert.js
+++ public/js/SimpleAlert.js
@@ -18,12 +18,26 @@ class SimpleAlert {
         let list = ``;
       
         if(obj.message.length > 0){
+            if(obj.express){
+                console.log('yessssss')
+                for (let ele of  obj.message){
+                    console.log('listttttt', ele.msg)
+                        list += `<li>${ele.msg}</li>`
+                    
+                }
+                
+            }
+            else{
+            console.log('noooooo')
             for (let ele of  obj.message){
                 for (let key in ele){
                     list += `<li>${key}${ele[key]}</li>`
                 }
             }
         }
+        }
+
+       
         alertElem.innerHTML = `
                 <div class="header d-flex justify-content-between align-item-center" >
                     <h4 class='d-flex align-item-center'><i class="fa-solid fa-circle-exclamation me-3"></i> <p class="title">${obj.title}</p></h4>
@@ -52,7 +66,7 @@ class SimpleAlert {
         this.positiveCallbackFn = obj.buttons.positive.action;
         this.negativeCallbackFn = obj.buttons.negative.action;
 
-        this.initEvents();
+        this.initEvents(obj);
 
     }
 
@@ -66,7 +80,7 @@ class SimpleAlert {
         }
     }
 
-    initEvents() {
+    initEvents(obj) {
 
         this.closeElem.addEventListener('click', () => {
             this.removeAlert();
@@ -74,7 +88,9 @@ class SimpleAlert {
 
         this.positiveElem.addEventListener('click', () => {
             this.removeAlert();
-            // location.reload();
+            if(obj.type == 'alert-success'){
+            location.reload();
+            }
         })
 
         this.negativeElem.addEventListener('click', () => {
