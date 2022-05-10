4cf23b9c625c7085efaf16d7c96e11cdb0c0fca0
Kapil Sharma <nmimsportalteam@gmail.com>
2022-05-10 12:51:57 +0530

    changes parameter of sp

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const {
 
 
 const options = {
-    pfx: readFileSync('D:/Suraj Projects/INFRA/infra_v2/cert/server.pfx'),
+    pfx: readFileSync('D:/INFRA-2022/infra_v2/cert/server.pfx'),
     passphrase: 'time#2021'
  };
  
