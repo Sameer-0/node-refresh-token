626bf8a19945197f50410dfe148921416227c550
Rajesh <pillai.rajesh7@gmail.com>
2022-05-11 14:13:25 +0530

    testig

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     // SIDE-BAR OPENING EVENT
-    const mobileHamburger = document.querySelector('.hamburger-smalldevices');
-    const leftBar = document.querySelector('.left-sidebar');
+   
+})
 
-    mobileHamburger.addEventListener('click', () => {
-        leftBar.classList.toggle('left-sidebar-open');
-    })
+const mobileHamburger = document.querySelector('.hamburger-smalldevices');
+const leftBar = document.querySelector('.left-sidebar');
+
+mobileHamburger.addEventListener('click', () => {
+leftBar.classList.toggle('left-sidebar-open');
 })
 
 
