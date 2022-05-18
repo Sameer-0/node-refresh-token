3ab8754f8a5e5c264e7e333ef154d0442096d078
Rajesh <pillai.rajesh7@gmail.com>
2022-05-18 17:49:11 +0530

    Faculty date time setting table  update

--- app/controllers/admin/faculty/dateTimes.js
+++ app/controllers/admin/faculty/dateTimes.js
@@ -32,6 +32,9 @@ module.exports = {
         }
 
         FacultyDateTimes.save(object, res.locals.slug, res.locals.userId).then(result => {
+            if (req.body.settingName) {
+                Settings.updateByName(res.locals.slug, req.body.settingName)
+            }
             res.status(200).json(JSON.parse(result.output.output_json))
         }).catch(error => {
             console.log(error)
