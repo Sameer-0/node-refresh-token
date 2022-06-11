13f46600ea81d2200bebc2c69915a82f77c33133
technologydynamite@gmail.com <suraj.cartgeek@gmail.com>
2022-06-11 12:03:49 +0530

    divion chnages

--- app/models/Divisions.js
+++ app/models/Divisions.js
@@ -15,8 +15,12 @@ module.exports = class Divisions {
 
     static fetchAll(rowcount, slug) {
         return poolConnection.then(pool => {
-            return pool.request().query(`SELECT TOP ${Number(rowcount)} d.id, d.division, d.division_num, d.division_count, IIF(d.count_for_theory_batch IS NULL , 0, d.count_for_theory_batch) AS count_for_theory_batch, IIF(d.count_for_practical_batch IS NULL , 0 , d.count_for_practical_batch) AS count_for_practical_batch, IIF(d.count_for_tutorial_batch IS NULL ,0, d.count_for_tutorial_batch) AS count_for_tutorial_batch, IIF(d.count_for_workshop_batch IS NULL ,0 , d.count_for_workshop_batch) AS count_for_workshop_batch, icw.module_name
-            FROM [${slug}].divisions d INNER JOIN [${slug}].initial_course_workload icw ON icw.id = d.course_lid ORDER BY d.division_num ASC`)
+            return pool.request().query(`SELECT TOP ${Number(rowcount)} d.id, d.division, d.division_num, d.division_count, p.program_name, ads.acad_session, IIF(d.count_for_theory_batch IS NULL , 0, d.count_for_theory_batch) AS count_for_theory_batch, IIF(d.count_for_practical_batch IS NULL , 0 , d.count_for_practical_batch) AS count_for_practical_batch, IIF(d.count_for_tutorial_batch IS NULL ,0, d.count_for_tutorial_batch) AS count_for_tutorial_batch, IIF(d.count_for_workshop_batch IS NULL ,0 , d.count_for_workshop_batch) AS count_for_workshop_batch, icw.module_name
+            FROM [${slug}].divisions d 
+            INNER JOIN [${slug}].initial_course_workload icw ON icw.id = d.course_lid
+            INNER JOIN [${slug}].programs p ON p.program_id = icw.program_id
+            INNER JOIN [dbo].acad_sessions ads ON ads.id = icw.acad_session_lid
+            ORDER BY d.division_num ASC`)
         })
     }
 
@@ -44,16 +48,27 @@ module.exports = class Divisions {
         return poolConnection.then(pool => {
             let request = pool.request()
             return request.input('pageNo', sql.Int, pageNo)
-                .query(`SELECT  d.id, d.division, d.division_num, d.division_count, IIF(d.count_for_theory_batch IS NULL , 0, d.count_for_theory_batch) AS count_for_theory_batch, IIF(d.count_for_practical_batch IS NULL , 0 , d.count_for_practical_batch) AS count_for_practical_batch, IIF(d.count_for_tutorial_batch IS NULL ,0, d.count_for_tutorial_batch) AS count_for_tutorial_batch, IIF(d.count_for_workshop_batch IS NULL ,0 , d.count_for_workshop_batch) AS count_for_workshop_batch, icw.module_name
-                FROM [${slug}].divisions d INNER JOIN [${slug}].initial_course_workload icw ON icw.id = d.course_lid ORDER BY d.id DESC  OFFSET (@pageNo - 1) * 10 ROWS FETCH NEXT 10 ROWS ONLY`)
+                .query(`SELECT  d.id, d.division, d.division_num, d.division_count,p.program_name, ads.acad_session, IIF(d.count_for_theory_batch IS NULL , 0, d.count_for_theory_batch) AS count_for_theory_batch, IIF(d.count_for_practical_batch IS NULL , 0 , d.count_for_practical_batch) AS count_for_practical_batch, IIF(d.count_for_tutorial_batch IS NULL ,0, d.count_for_tutorial_batch) AS count_for_tutorial_batch, IIF(d.count_for_workshop_batch IS NULL ,0 , d.count_for_workshop_batch) AS count_for_workshop_batch, icw.module_name
+                FROM [${slug}].divisions d 
+                INNER JOIN [${slug}].initial_course_workload icw ON icw.id = d.course_lid
+                INNER JOIN [${slug}].programs p ON p.program_id = icw.program_id
+                INNER JOIN [dbo].acad_sessions ads ON ads.id = icw.acad_session_lid ORDER BY d.id DESC  OFFSET (@pageNo - 1) * 10 ROWS FETCH NEXT 10 ROWS ONLY`)
         })
     }
 
     static search(rowcount, keyword, slug) {
         return poolConnection.then(pool => {
             return pool.request().input('keyword', sql.NVarChar(100), '%' + keyword + '%')
-                .query(`SELECT TOP ${Number(rowcount)} d.id, d.division, d.division_num, d.division_count, IIF(d.count_for_theory_batch IS NULL , 0, d.count_for_theory_batch) AS count_for_theory_batch, IIF(d.count_for_practical_batch IS NULL , 0 , d.count_for_practical_batch) AS count_for_practical_batch, IIF(d.count_for_tutorial_batch IS NULL ,0, d.count_for_tutorial_batch) AS count_for_tutorial_batch, IIF(d.count_for_workshop_batch IS NULL ,0 , d.count_for_workshop_batch) AS count_for_workshop_batch, icw.module_name
-                FROM [${slug}].divisions d INNER JOIN [${slug}].initial_course_workload icw ON icw.id = d.course_lid WHERE  d.division LIKE @keyword OR d.division_count LIKE @keyword OR d.division_num LIKE @keyword OR d.count_for_theory_batch LIKE @keyword OR d.count_for_tutorial_batch LIKE @keyword OR d.count_for_workshop_batch LIKE @keyword OR icw.module_name LIKE @keyword ORDER BY d.id DESC`)
+                .query(`SELECT TOP ${Number(rowcount)} d.id, d.division, d.division_num, d.division_count, p.program_name, ads.acad_session, IIF(d.count_for_theory_batch IS NULL , 0, d.count_for_theory_batch) AS count_for_theory_batch, IIF(d.count_for_practical_batch IS NULL , 0 , d.count_for_practical_batch) AS count_for_practical_batch, IIF(d.count_for_tutorial_batch IS NULL ,0, d.count_for_tutorial_batch) AS count_for_tutorial_batch, IIF(d.count_for_workshop_batch IS NULL ,0 , d.count_for_workshop_batch) AS count_for_workshop_batch, icw.module_name
+                FROM [${slug}].divisions d 
+                INNER JOIN [${slug}].initial_course_workload icw ON icw.id = d.course_lid
+                INNER JOIN [${slug}].programs p ON p.program_id = icw.program_id
+                INNER JOIN [dbo].acad_sessions ads ON ads.id = icw.acad_session_lid 
+                WHERE  d.division LIKE @keyword OR d.division_count LIKE @keyword 
+                OR d.division_num LIKE @keyword OR d.count_for_theory_batch LIKE @keyword 
+                OR d.count_for_tutorial_batch LIKE @keyword 
+                OR d.count_for_workshop_batch LIKE @keyword 
+                OR icw.module_name LIKE @keyword OR p.program_name LIKE @keyword OR ads.acad_session LIKE @keyword ORDER BY d.id DESC`)
         })
     }
 
