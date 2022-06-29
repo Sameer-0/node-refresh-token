4e7c81c79e6307ca575ba4b0411d2524e9ede166
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-06-29 16:19:14 +0530

    school setting syntax change

--- app/controllers/admin/timeTableSimulation/timetable.js
+++ app/controllers/admin/timeTableSimulation/timetable.js
@@ -16,22 +16,20 @@ module.exports = {
                 Rooms.fetchBookedRooms(res.locals.organizationId),
                 Days.fetchActiveDay(res.locals.slug),
                 TimeTable.getPendingEventPrograms(res.locals.slug),
-               
-                
             ])
             .then(result => {
-                console.log('pending programs list::::',  result[3].recordset)
+                console.log('pending programs list::::', result[3].recordset)
                 res.render('admin/timeTableSimulation/timetable', {
                     programList: result[0].recordset,
                     programListJson: JSON.stringify(result[0].recordset),
                     roomList: JSON.stringify(result[1].recordset),
                     dayList: result[2].recordset,
                     pendingEventPrograms: JSON.stringify(result[3].recordset),
-                    
+
                     breadcrumbs: req.breadcrumbs,
                     Url: req.originalUrl
                 })
-            }).catch( error => {
+            }).catch(error => {
                 console.log('error', error)
             })
     },
@@ -51,11 +49,13 @@ module.exports = {
     },
 
     getEventsByProgramSessionDay: (req, res, next) => {
+        console.log('req::::::::::::>', req.body)
         Promise.all([
             TimeTable.getEventsByProgramSessionDay(res.locals.slug, req.body.dayLid, req.body.programLid, req.body.acadSessionLid),
             TimeTable.getEventsByProgramSessionDay(res.locals.slug, req.body.dayLid),
             SchoolTimings.getTimeTableSimulationSlots(res.locals.slug, req.body.dayLid, req.body.programLid, req.body.acadSessionLid)
         ]).then(results => {
+            console.log('results::::::::::', results[0], results[1])
             res.status(200).send({
                 eventList: results[0].recordset,
                 allEventList: results[1].recordset,
@@ -67,7 +67,7 @@ module.exports = {
     getPendingEvents: (req, res, next) => {
         console.log('pending req', req.body)
         TimeTable.getPendingEvents(res.locals.slug, req.body.programLid, req.body.acadSessionLid).then(result => {
-           console.log('pending list:::', result.recordset)
+            console.log('pending list:::', result.recordset)
             res.status(200).send(result.recordset)
         })
     },
@@ -75,13 +75,13 @@ module.exports = {
     getPendingEventSessions: (req, res, next) => {
         console.log('pending req', req.body)
         TimeTable.getPendingEventSessions(res.locals.slug, req.body.programLid).then(result => {
-           console.log('pending event session list:::', result.recordset)
+            console.log('pending event session list:::', result.recordset)
             res.status(200).send(result.recordset)
         })
     },
 
 
-//Implemented in timetablesocket
+    //Implemented in timetablesocket
     dropEvent: (req, res, next) => {
         console.log(req.body);
         TimeTable.dropEvent(res.locals.slug, res.locals.userId, req.body.eventLid).then(result => {
@@ -89,22 +89,23 @@ module.exports = {
         })
     },
 
-//Implemented in timetablesocket
+    //Implemented in timetablesocket
     scheduleEvent: (req, res) => {
-        let object = { 
-            allocate_events: JSON.parse(req.body.inputJSON) 
+        let object = {
+            allocate_events: JSON.parse(req.body.inputJSON)
         }
-        
+
         TimeTable.scheduleEvent(res.locals.slug, res.locals.userId, object).then(result => {
             res.status(200).json(JSON.parse(result.output.output_json))
         }).catch(error => {
-            if(isJsonString.isJsonString(error.originalError.info.message)){
+            if (isJsonString.isJsonString(error.originalError.info.message)) {
                 res.status(500).json(JSON.parse(error.originalError.info.message))
-            }
-            else{
-                res.status(500).json({status:500,
-                description:error.originalError.info.message,
-                data:[]})
+            } else {
+                res.status(500).json({
+                    status: 500,
+                    description: error.originalError.info.message,
+                    data: []
+                })
             }
         })
     },
@@ -112,41 +113,46 @@ module.exports = {
 
     //Implemented in timetablesocket
     swapEvents: (req, res) => {
-        let object = { 
+        let object = {
             "swap_events": JSON.parse(req.body.inputJSON)
         }
-        
+
         TimeTable.swapEvents(res.locals.slug, res.locals.userId, object).then(result => {
             console.log('successfully swaped', result)
             res.status(200).json(JSON.parse(result.output.output_json))
         }).catch(error => {
             console.log('error::::::', error);
-            if(isJsonString.isJsonString(error.originalError.info.message)){
+            if (isJsonString.isJsonString(error.originalError.info.message)) {
                 res.status(500).json(JSON.parse(error.originalError.info.message))
-            }
-            else{
-                res.status(500).json({status:500,
-                description:error.originalError.info.message,
-                data:[]})
+            } else {
+                res.status(500).json({
+                    status: 500,
+                    description: error.originalError.info.message,
+                    data: []
+                })
             }
         })
     },
 
 
 
-    allocateFaculties: (req, res) => {        
+    allocateFaculties: (req, res) => {
         console.log('faculty allocation request', req.body)
         TimeTable.allocateFaculties(res.locals.slug, req.body).then(result => {
 
-            res.status(200).json({description: "Successful!"})
+            res.status(200).json({
+                description: "Successful!"
+            })
         })
     },
 
-    deallocateFaculties: (req, res) => {        
+    deallocateFaculties: (req, res) => {
         console.log('faculty deallocation request', req.body)
         TimeTable.deallocateFaculties(res.locals.slug, req.body).then(result => {
 
-            res.status(200).json({description: "Successful!"})
+            res.status(200).json({
+                description: "Successful!"
+            })
         })
     },
 }
