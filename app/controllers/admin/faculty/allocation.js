f17ab7bfdbe0a05ed0ee3a29bb989d7eafd85d7e
surajurbanledger <suraj.vishwakarma@urbanledger.app>
2022-07-03 19:38:10 +0530

    faculty allocation

--- app/controllers/admin/faculty/allocation.js
+++ app/controllers/admin/faculty/allocation.js
@@ -4,12 +4,17 @@ const {
     validationResult
 } = require('express-validator');
 const Faculties = require('../../../models/Faculties')
+const Days = require('../../../models/Days')
+const SlotIntervalTiming = require('../../../models/SlotIntervalTimings')
+
 module.exports = {
 
     getPage: (req, res) => {
-        Promise.all([Faculties.fetchAll(10000, res.locals.slug)]).then(result => {
+        Promise.all([Faculties.fetchAll(10000, res.locals.slug), Days.fetchAll(10, res.locals.slug), SlotIntervalTiming.slotTimesForSchoolTiming(res.locals.slug)]).then(result => {
             res.render('admin/faculty/facultyallocationstatus', {
                 facultyList: result[0].recordset,
+                dayList: JSON.stringify(result[1].recordset),
+                timeSlotList:JSON.stringify(result[2].recordset),
                 breadcrumbs: req.breadcrumbs,
                 Url: req.originalUrl
             })
@@ -17,11 +22,13 @@ module.exports = {
     },
 
     checkfacultyStatus: (req, res, next) => {
-        Faculties.allocationStatus(res.locals.slug, req.body.faculty_id).then(result => {
+        console.log('BODY::::::>>',req.body)
+        Promise.all([Days.fetchAll(10, res.locals.slug),  Faculties.allocationStatus(res.locals.slug, req.body.faculty_id)]).then(result => {
             res.status(200).json({
                 status: 200,
                 message: "success",
-                facultyStatus: result.recordset
+                daysdata: result[0].recordset,
+                facultyallocations: result[1].recordset
             })
         })
 
