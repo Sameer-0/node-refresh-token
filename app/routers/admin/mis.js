7d4de48716a4e3c8f434a933b4e08d2c94043f26
Suraj Vishwakarma <suraj.vishwakarma.EXT@nmims.edu>
2022-07-21 17:54:46 +0530

    room wis

--- app/routers/admin/mis.js
+++ app/routers/admin/mis.js
@@ -8,10 +8,12 @@ const {
 const index =  require('../../controllers/admin/mis/index')
 const pivoted =  require('../../controllers/admin/mis/pivoted')
 const facultydaywise =  require('../../controllers/admin/mis/facultydaywise')
-
+const roomwiseallocation =  require('../../controllers/admin/mis/roomwiseallocation')
 
 router.get('/mis/simulation-chart', index.getPage) 
 router.get('/mis/simulation-chart/pivoted', pivoted.getPage) 
 router.get('/mis/simulation-chart/faculty-day-wise', facultydaywise.getPage) 
 router.post('/mis/simulation-chart/faculty-day-wise', facultydaywise.facultyDayWise) 
+router.get('/mis/simulation-chart/room-wise-allocation', roomwiseallocation.getPage) 
+router.post('/mis/simulation-chart/room-wise-allocation', roomwiseallocation.getRoomAllocation) 
 module.exports = router;
